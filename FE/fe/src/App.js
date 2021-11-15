bbc1a9b2f7250bf13e71ac3f7c6cd8ddcfb0cdd7
AmandaCallender <58009118+AmandaCallender@users.noreply.github.com>
2021-11-14 22:18:27 -0500

    Created React App

---
+++ FE/fe/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Farm Sim Ranker</h1>
+        <h2>A site where you can rank your favorite farming simulation games</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
